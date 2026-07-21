4c9110f7fd27c465eab72fadfe1d43ec125293e3
KariaKoegler <koeglerkaria@gmail.com>
2026-07-21 14:30:53 +0200

    Update 3_merge.R

--- 3_merge.R
+++ 3_merge.R
@@ -18,41 +18,15 @@ df <- df %>%
   dplyr::select(reben)
 
 shapefile <- cbind(shapefile, df)
-#shapefile <- shapefile[shapefile$reben != 0,]
+shapefile <- shapefile[shapefile$reben != 0,]
 
-#Some entries overlap as a part of the vineyard is more steep or has more
-#biodiversity as others
-#before running this line, check whether the server can handle it (too many
-#users at the same time make it crash)
-#shapefile <- st_difference(shapefile)
+#deleting unnecessary columns
+shapefile <- shapefile[, -c(1, 2, 6:22, 24: 34)]
+
 
 # 3. Merging of shapefile and Rebbaukataster ######################################
-data_long[1,]
-shapefile[1,]
-
 shapefile <- shapefile %>%
   rename(Rebgemeinde = GEMBEZ, Parzellennr. = ESTATE_NR)
-
-#Excel kann keine Liste in den Koordinaten haben, deshalb neue Spalten mit x und y
-# shapefileOhnePolygon <- shapefile
-# shapefileOhnePolygon$x <- sub(",.*", "", shapefileOhnePolygon$geometry)
-# shapefileOhnePolygon$y <- sub(".*,", "", shapefileOhnePolygon$geometry)
-# 
-# shapefileOhnePolygon$x <- substring(shapefileOhnePolygon$x, 13)
-# shapefileOhnePolygon$y <- substring(shapefileOhnePolygon$y, 1, nchar(shapefileOhnePolygon$y)-3)
-# shapefileOhnePolygon <- st_drop_geometry(shapefileOhnePolygon)
-
-#komische Parzellennummern werden korrigiert
-# shapefileOhnePolygon <- shapefileOhnePolygon %>%
-#  mutate(Parzellennr. = as.numeric(gsub("\\D", "", Parzellennr.)))
-
-#Zusammengefügte Tabelle ohne Polygons aber mit Koordinaten für Excel
-# merged_data_ohne_polygon <- data_long %>%
-#   full_join(shapefileOhnePolygon, by = c("Parzellennr.", "Rebgemeinde"))
-
-# Als Excel speichern
-#library(openxlsx)
-#write.xlsx(merged_data_ohne_polygon, file = "MergeErsterVersuch.xlsx")
 
 #Merge of data_long to shapefile
 data_long <- data_long %>%
@@ -62,5 +36,5 @@ shapefile <- shapefile %>%
 shapefile <- shapefile %>%
   distinct(Parzellennr., Rebgemeinde, .keep_all = TRUE)
 
-merged_data_all <- data_long %>%
-  full_join(shapefile, by = c("Parzellennr.", "Rebgemeinde"))
+merged_data_all_original <- full_join(data_long, shapefile) 
+# View(merged_data_all_original)
