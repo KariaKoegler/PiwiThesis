41af521fa6073254f922d2de9d2ea76b7aef8cef
KariaKoegler <koeglerkaria@gmail.com>
2026-07-21 16:10:52 +0200

    Update 7_clustering_and_regression.R

--- 7_clustering_and_regression.R
+++ 7_clustering_and_regression.R
@@ -2,86 +2,49 @@
 #install.packages("multiwayvcov")
 #install.packages("lmtest")
 #install.packages("stargazer")
+#install.packages("stats")
 library(multiwayvcov)
 library(lmtest)
 library(sandwich)
 library(stargazer)
+library(tidyr)
+library(stats)
 
-View(merged_data_all_haildays)
-View(merged_data_all_hailsize)
+#delete NAs in Betrieb
+merged_data_all_haildays_2cm <- merged_data_all_haildays_2cm[!is.na(merged_data_all_haildays_2cm$Betrieb),]
+merged_data_all_hailsize <- merged_data_all_hailsize[!is.na(merged_data_all_hailsize$Betrieb),]
+merged_data_all_haildays <- merged_data_all_haildays[!is.na(merged_data_all_haildays$Betrieb),]
 
-#2. Preparation ################################################################
-merged_data_all_haildays$Weinmerkmal <- ifelse(merged_data_all_haildays$Weinmerkmal == "interspezifisch", 1,
-                         ifelse(merged_data_all_haildays$Weinmerkmal == "europäisch", 0, NA))
 
-merged_data_all_hailsize$Weinmerkmal <- ifelse(merged_data_all_hailsize$Weinmerkmal == "interspezifisch", 1,
-                                               ifelse(merged_data_all_hailsize$Weinmerkmal == "europäisch", 0, NA))
-
-#Betrieb as factor
-merged_data_all_hailsize$Betrieb <- as.factor(merged_data_all_hailsize$Betrieb)
-merged_data_all_haildays$Betrieb <- as.factor(merged_data_all_haildays$Betrieb)
-
-#delete entries where reben = 0
-merged_data_all_haildays = merged_data_all_haildays[merged_data_all_haildays$reben > 0, ]
-#remove column reben
-merged_data_all_haildays <- merged_data_all_haildays[,-c(10)]
-
-merged_data_all_hailsize = merged_data_all_hailsize[merged_data_all_hailsize$reben > 0, ]
-merged_data_all_hailsize <- merged_data_all_hailsize[,-c(10)]
-dim(merged_data_all_haildays)
-
-#delete Nas
-merged_data_all_haildays <- merged_data_all_haildays[!is.na(merged_data_all_haildays$sumOfHaildays_2cm),]
-
-#there are no datapoints with haildays left, so it never hailed larger than 2 cm
-#in Zurich during the time frame
-
-#test: Other variables work
-summary(lm(Weinmerkmal ~ plantation_year, data = merged_data_all_haildays)) #the later it was planted,
-#the more piwi -> time fixed effect
-summary(lm(Weinmerkmal ~ year, data = merged_data_all_haildays))
-summary(lm(Weinmerkmal ~ sumOfHaildays_2cm, data = merged_data_all_haildays))
-
-view(merged_data_all_hailsize)
-
-summary(lm(Weinmerkmal ~ meanHailsize + Hagelgross, data = merged_data_all_hailsize))
-summary(lm(Weinmerkmal ~ Hagelgross + plantation_year + Betrieb, data = merged_data_all_hailsize))
-summary(lm(Weinmerkmal ~ meanHailsize + Hagelgross + Betrieb + plantation_year, data = merged_data_all_hailsize))
-
-merged_data_all_hailsize$plantation_year
-
-# Run rergession
+#2. Run rergession ##############################################################
 models <- list(
-  #m1 <- lm(Weinmerkmal ~ sumOfHaildays_2cm, data = merged_data_all_haildays),
-  
-  m2 <- lm(Weinmerkmal ~ meanHailsize + Hagelgross, data = merged_data_all_hailsize),
-  m3 <- lm(Weinmerkmal ~ Hagelgross + plantation_year, data = merged_data_all_hailsize),
-  m4 <- lm(Weinmerkmal ~ Hagelgross + Betrieb, data = merged_data_all_hailsize),
-  m5 <- lm(Weinmerkmal ~ Hagelgross + Betrieb + plantation_year, data = merged_data_all_hailsize),
-  m6 <- lm(Weinmerkmal ~ meanHailsize + Betrieb + plantation_year, data = merged_data_all_hailsize)
+  m1 <- lm(Weinmerkmal ~ RowMens, data = merged_data_all_haildays_2cm),
+  m2 <- lm(Weinmerkmal ~ RowMens + Betrieb + plantation_year, data = merged_data_all_haildays_2cm),
+  m3 <- lm(Weinmerkmal ~ RowMens , data = merged_data_all_hailsize),
+  m4 <- lm(Weinmerkmal ~ RowMens + plantation_year + Betrieb, data = merged_data_all_hailsize),
+  m5 <- lm(Weinmerkmal ~ RowMens, data = merged_data_all_haildays),
+  m6 <- lm(Weinmerkmal ~ RowMens + plantation_year + Betrieb, data = merged_data_all_haildays)
 )
 
 # Here wird nach der Variable id (Betriebsid geclustert)
 
 clwb <- list(
-  #se1 <- vcovCL(models[[1]], cluster = ~ Betrieb),
-  se2 <- vcovCL(models[[1]], cluster = ~ Betrieb),
-  se3 <- vcovCL(models[[2]], cluster = ~ Betrieb),
-  se4 <- vcovCL(models[[3]], cluster = ~ Betrieb),
-  se5 <- vcovCL(models[[4]], cluster = ~ Betrieb),
-  se6 <- vcovCL(models[[5]], cluster = ~ Betrieb)
+  se1 <- vcovCL(models[[1]], cluster = ~ Betrieb),
+  se2 <- vcovCL(models[[2]], cluster = ~ Betrieb),
+  se3 <- vcovCL(models[[3]], cluster = ~ Betrieb),
+  se4 <- vcovCL(models[[4]], cluster = ~ Betrieb),
+  se5 <- vcovCL(models[[5]], cluster = ~ Betrieb),
+  se6 <- vcovCL(models[[6]], cluster = ~ Betrieb)
 )
 
 # Und dann der SE auf signifikanz getestet
 ct <- list(
-  
-  #ct1 <- coeftest(models[[1]], vcov = clwb[[1]])[, 2],
-  ct2 <- coeftest(models[[1]], vcov = clwb[[1]])[, 2],
-  ct3 <- coeftest(models[[2]], vcov = clwb[[2]])[, 2],
-  ct4 <- coeftest(models[[3]], vcov = clwb[[3]])[, 2],
-  ct5 <- coeftest(models[[4]], vcov = clwb[[4]])[, 2],
-  ct6 <- coeftest(models[[5]], vcov = clwb[[5]])[, 2]
-  
+  ct1 <- coeftest(models[[1]], vcov = clwb[[1]])[, 2],
+  ct2 <- coeftest(models[[2]], vcov = clwb[[2]])[, 2],
+  ct1 <- coeftest(models[[3]], vcov = clwb[[3]])[, 2],
+  ct1 <- coeftest(models[[4]], vcov = clwb[[4]])[, 2],
+  ct1 <- coeftest(models[[5]], vcov = clwb[[5]])[, 2],
+  ct1 <- coeftest(models[[6]], vcov = clwb[[6]])[, 2]
 )
 
 ## Export as word
@@ -99,9 +62,9 @@ stargazer(c(models),
           
           omit = c('[C][o][n][s][t][a][n][t]','[B][e][t][r][i][e][b]'),
           
-          add.lines=list(c('Time fixed effects','No','Yes', 'No', 'Yes', 'Yes'),
+          add.lines=list(c('Time fixed effects','No', 'Yes', 'No', 'Yes', 'No', 'Yes'),
                          
-                         c('Farm fixed effects','No','No', 'Yes', 'Yes', 'Yes')),
+                         c('Farm fixed effects', 'No', 'Yes', 'No', 'Yes', 'No', 'Yes')),
           
           notes.align = "l",
           
@@ -121,6 +84,6 @@ stargazer(c(models),
           
           font.size = "tiny",
           
-          out="Y:/27_cadaster_canton_zurich/27_cadaster_canton_zurich/3_output/Regression_HS_vs_HD.doc")
+          out="Y:/27_cadaster_canton_zurich/27_cadaster_canton_zurich/3_output/test.doc")
 
 
